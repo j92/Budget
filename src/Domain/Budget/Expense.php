e11f9b22df665708136c9a30a9208df58779c523
Joost van Driel <joostvdriel@gmail.com>
2017-02-26 10:46:57 +0100

    Added 2 domain classes

---
+++ src/Domain/Budget/Expense.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Domain\Budget;
+
+/**
+ * Class Expense
+ * @package Domain\Budget
+ *
+ * An expense is an outgoing transaction
+ */
+class Expense
+{
+
+}
---
+++ src/Domain/Budget/Income.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Domain\Budget;
+
+/**
+ * Class Income
+ * @package Domain\Budget
+ *
+ * An income is an incoming transaction
+ */
+class Income
+{
+
+}
